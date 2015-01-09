520c1ac69e4e76278039e36722f49baccb69eb4c
Diego De Souza <diegotrus7@gmail.com>
2015-01-08 21:43:39 -0500

    user is able to see a specific listing

--- spec/features/places/user_sees_all_listing_spec.rb
+++ spec/features/places/user_sees_all_listing_spec.rb
@@ -8,8 +8,8 @@ So that i can know what listings are available for rent
 
 Acceptance Criteria
 
-[ ] I must be able to see all listings from index page
-[ ] I must be able to navigate to a single listing
+[X] I must be able to see all listings from index page
+[X] I must be able to navigate to a single listing
 ) do
   before :each do
     @user = create(:user)
@@ -17,14 +17,24 @@ Acceptance Criteria
     @listing1 = create(:listing)
   end
 
-  scenario "user sees all listings", focus: true do
+  scenario "user sees all listings" do
 
    sign_in_as(@user)
 
    visit root_path
 
-   save_and_open_page
    expect(page).to have_content @listing.title
    expect(page).to have_content @listing1.title
+  end
+
+  scenario "user navigates to show page" do
+
+    sign_in_as(@user)
+
+    visit root_path
+
+    click_on @listing.title
+
+    current_path.should == listing_path(@listing)
   end
 end
