170de67d48296979ee8e00b166039ff027a45978
Diego De Souza <diegotrus7@gmail.com>
2015-01-21 16:15:21 -0500

    made changes to show page, index, flash notices created images model for uploading in the show

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150114201058) do
+ActiveRecord::Schema.define(version: 20150121195854) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "images", force: :cascade do |t|
+    t.string "image1"
+    t.string "image2"
+    t.string "image3"
+    t.string "image4"
+    t.string "image5"
+  end
 
   create_table "listings", force: :cascade do |t|
     t.string   "title",                                        null: false
